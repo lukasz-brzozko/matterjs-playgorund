f4653a6ade8d188c5fd8f499f1a80dfd03fae6a4
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-25 18:38:11 +0100

    feat: add MouseConstraint component

---
+++ src/components/MouseConstraintC/MouseConstraintC.ts
@@ -0,0 +1,36 @@
+import {
+  Engine, IMouseConstraintDefinition, Mouse, MouseConstraint,
+} from 'matter-js';
+
+interface MouseConstraintInterface {
+  engine: Engine;
+  mouse: Mouse;
+}
+
+class MouseConstraintC implements MouseConstraintInterface {
+  engine: Engine;
+
+  mouse: Mouse;
+
+  constructor(engine: Engine, mouse: Mouse) {
+    this.engine = engine;
+    this.mouse = mouse;
+  }
+
+  create(): MouseConstraint {
+    const options: IMouseConstraintDefinition = {
+      mouse: this.mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+        damping: 0.1,
+      },
+    };
+
+    return MouseConstraint.create(this.engine, options);
+  }
+}
+
+export default MouseConstraintC;
