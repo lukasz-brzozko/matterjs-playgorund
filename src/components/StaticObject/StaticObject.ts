1831172a2e2ed8a507e2ee5debd520cf989fb4e0
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-26 17:48:14 +0100

    feat: add WallGenerator component

---
+++ src/components/StaticObject/StaticObject.ts
@@ -0,0 +1,44 @@
+import {
+  Bodies, Body, IChamferableBodyDefinition, World,
+} from 'matter-js';
+
+interface StaticObjectInterface {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  world: World;
+}
+
+class StaticObject implements StaticObjectInterface {
+  x: number;
+
+  y: number;
+
+  w: number;
+
+  h: number;
+
+  world: World;
+
+  constructor(x: number, y: number, w: number, h: number, world: World) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.world = world;
+  }
+
+  create(): Body {
+    const {
+      x, y, w, h,
+    } = this;
+    const options: IChamferableBodyDefinition = {
+      isStatic: true,
+    };
+
+    return Bodies.rectangle(x, y, w, h, options);
+  }
+}
+
+export default StaticObject;
