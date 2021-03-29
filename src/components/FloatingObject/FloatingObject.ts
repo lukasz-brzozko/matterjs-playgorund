9c7ff416dd8b2cba1c8a633bef3289de2fe3bbce
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-29 19:46:18 +0200

    feat: add random values function

--- src/components/FloatingObject/FloatingObject.ts
+++ src/components/FloatingObject/FloatingObject.ts
@@ -1,20 +1,28 @@
-import { Bodies, Body, World } from 'matter-js';
+import {
+  Bodies, Body, IChamferableBodyDefinition, World,
+} from 'matter-js';
+
+import COLORS from '../../constants/colors';
+import randomValue from '../../helpers/randomValue';
 
 interface FloatingObjectInterface {
   x: number;
   y: number;
   r: number;
   world: World;
+  colors: typeof COLORS;
 }
 
 class FloatingObject implements FloatingObjectInterface {
-  x: number;
+  x;
 
-  y: number;
+  y;
 
-  r: number;
+  r;
 
-  world: World;
+  world;
+
+  colors = COLORS;
 
   constructor(x: number, y: number, r: number, world: World) {
     this.x = x;
@@ -23,8 +31,44 @@ class FloatingObject implements FloatingObjectInterface {
     this.world = world;
   }
 
+  generateColors(): string {
+    const colorNames = Object.keys(this.colors);
+    const min = 0;
+    const max = colorNames.length;
+    const randomColorIndex = randomValue(min, max);
+    const chosenColorName = colorNames[randomColorIndex];
+    const colorVal = this.colors[chosenColorName];
+
+    return colorVal;
+  }
+
   create(): Body {
-    return Bodies.circle(this.x, this.y, this.r);
+    const randomColor = this.generateColors();
+    const options: IChamferableBodyDefinition = {
+      density: 0.1,
+      frictionAir: 0.005,
+      mass: 10,
+      render: {
+        fillStyle: 'transparent',
+        lineWidth: window.innerWidth > 700 ? 3 : 2,
+        strokeStyle: randomColor,
+      },
+      restitution: 1,
+    };
+
+    const ball = Bodies.polygon(this.x, this.y, 3, this.r, options);
+    const velocityX = randomValue(-2, 5);
+    const velocityY = randomValue(-2, 5);
+    const angularVelocity = randomValue(-0.05, 0.15, true);
+
+    const velocity = {
+      x: velocityX,
+      y: velocityY,
+    };
+
+    Body.setVelocity(ball, velocity);
+    Body.setAngularVelocity(ball, angularVelocity);
+    return ball;
   }
 }
 
