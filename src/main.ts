054fdb4b96484633c989cfc106f238565fb97b56
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-29 20:49:47 +0200

    feat: add random generating floating objects

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
 import {
-  Engine, Render, World,
+  Body, Engine, Render, World,
 } from 'matter-js';
 import { debounce } from 'debounce';
+
 import FloatingObject from './components/FloatingObject';
+import getDOMElementRects from './helpers/getDOMElementRects';
 import MouseC from './components/MouseC';
 import MouseConstraintC from './components/MouseConstraintC';
+import randomValue from './helpers/randomValue';
 import WallGenerator from './helpers/wallGenerator';
+import StaticObject from './components/StaticObject';
 
-let engine:Engine;
+let engine: Engine;
 let render: Render;
+let world: World;
 
 const setup = () => {
   // create an engine
   engine = Engine.create();
+
+  // assign thw World's instance to the variable
+  world = engine.world;
+
+  // set world's params
+  world.gravity.scale = 0;
 
   // create a renderer
   render = Render.create({
     engine,
     element: document.body,
     options: {
+      background: 'transparent',
       height: window.innerHeight,
       width: window.innerWidth,
       wireframes: false,
@@ -26,18 +38,34 @@ const setup = () => {
   });
 
   // create all of the bodies
-  const ball = new FloatingObject(100, 100, 50, engine.world).create();
-  const walls = new WallGenerator(engine.world).generate();
+  const floatingObjects: Body[] = [];
+  for (let index = 0; index < 5; index += 1) {
+    const r = window.innerWidth > 1024 ? 35 : 12;
+    const x = randomValue(r * 2, window.innerWidth - (r * 2));
+    const y = randomValue(r * 2, window.innerHeight - (r * 2));
+    floatingObjects.push(new FloatingObject(x, y, r, world).create());
+  }
+
+  const childrenRects = getDOMElementRects('.container');
+  const childrenBorders: Body[] = [];
+  childrenRects.forEach(({
+    x, y, width, height,
+  }) => {
+    const border = new StaticObject(x + width / 2, y + height / 2, width, height, world).create();
+    childrenBorders.push(border);
+  });
+
+  const walls = new WallGenerator(world).generate();
 
   // create mouse and mouseConstraint
-  const mouse = new MouseC(render.canvas, engine.world).create();
+  const mouse = new MouseC(render.canvas, world).create();
   const mouseConstraint = new MouseConstraintC(engine, mouse).create();
 
   // add all of the bodies to the world
-  World.add(engine.world, [ball, ...walls]);
+  World.add(world, [...floatingObjects, ...walls, ...childrenBorders]);
 
   // add mouseConstraint to the world
-  World.add(engine.world, mouseConstraint);
+  World.add(world, mouseConstraint);
 
   // run the engine
   Engine.run(engine);
@@ -47,7 +75,7 @@ const setup = () => {
 };
 
 const resetCanvas = () => {
-  World.clear(engine.world, false);
+  World.clear(world, false);
   Engine.clear(engine);
   Render.stop(render);
   if (render.canvas) {
