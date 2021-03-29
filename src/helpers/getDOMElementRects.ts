90ab47681e875dc76ed34a1367f65ef14994ceb8
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-29 20:49:19 +0200

    feat: add getting DOM elements rects function

---
+++ src/helpers/getDOMElementRects.ts
@@ -0,0 +1,10 @@
+const getDOMElementRects = (selector: string): DOMRect[] => {
+  const element: Element | null = document.querySelector(selector);
+  const children = element?.children;
+  const childrenArr: Element[] = children ? [...children] : [];
+  const params = childrenArr.map((el: Element) => el.getBoundingClientRect());
+
+  return params;
+};
+
+export default getDOMElementRects;
