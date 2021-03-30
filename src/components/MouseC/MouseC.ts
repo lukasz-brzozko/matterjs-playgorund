470f73f680ff17acc95a6a65eb8e837af995ce3a
Lukasz Brzozko <lukasz9099@gmail.com>
2021-03-30 19:16:50 +0200

    fix: type fix

--- src/components/MouseC/MouseC.ts
+++ src/components/MouseC/MouseC.ts
@@ -1,17 +1,17 @@
 import { Mouse, World } from 'matter-js';
 
 interface MouseInterface {
-  canvas: HTMLCanvasElement;
+  canvas: HTMLCanvasElement | null;
   world: World;
   create(): Mouse;
 }
 
 class MouseC implements MouseInterface {
-  canvas: HTMLCanvasElement;
+  canvas: HTMLCanvasElement | null;
 
   world: World;
 
-  constructor(canvas: HTMLCanvasElement, world: World) {
+  constructor(canvas: HTMLCanvasElement | null, world: World) {
     this.canvas = canvas;
     this.world = world;
   }
