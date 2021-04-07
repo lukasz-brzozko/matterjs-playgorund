dc02cf00e04b31bc741496d5fc40f6d9b53069d1
Lukasz Brzozko <lukasz9099@gmail.com>
2021-04-07 14:07:26 +0200

    fix: add ignore patterns and change quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parserOptions: {
-    project: ["./tsconfig.eslint.json"],
+    project: ['./tsconfig.eslint.json'],
   },
   env: {
     browser: true,
     node: true,
   },
   settings: {
-    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
   rules: {
-    "linebreak-style": [2, "windows"],
+    'linebreak-style': [2, 'windows'],
   },
+  ignorePatterns: ['.eslintrc.js']
 };
